dabe7285783e043a7fbb573dcb1c0c2f81b16b4b
Shantanudarne16 <113509833+Shantanudarne16@users.noreply.github.com>
2022-09-14 14:23:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,100 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+filename = 'cricket-score-predictor-lr-model.pkl'
+regressor = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        try: 
+            batting_team = request.form['batting-team']
+            if batting_team == 'Chennai Super Kings':
+                temp_array = temp_array + [1,0,0,0,0,0,0,0]
+            elif batting_team == 'Delhi Daredevils':
+                temp_array = temp_array + [0,1,0,0,0,0,0,0]
+            elif batting_team == 'Punjab Kings':
+                temp_array = temp_array + [0,0,1,0,0,0,0,0]
+            elif batting_team == 'Kolkata Knight Riders':
+                temp_array = temp_array + [0,0,0,1,0,0,0,0]
+            elif batting_team == 'Mumbai Indians':
+                temp_array = temp_array + [0,0,0,0,1,0,0,0]
+            elif batting_team == 'Rajasthan Royals':
+                temp_array = temp_array + [0,0,0,0,0,1,0,0]
+            elif batting_team == 'Royal Challengers Bangalore':
+                temp_array = temp_array + [0,0,0,0,0,0,1,0]
+            elif batting_team == 'Sunrisers Hyderabad':
+                temp_array = temp_array + [0,0,0,0,0,0,0,1]
+                
+                
+            bowling_team = request.form['bowling-team']
+            if bowling_team == 'Chennai Super Kings':
+                temp_array = temp_array + [1,0,0,0,0,0,0,0]
+            elif bowling_team == 'Delhi Daredevils':
+                temp_array = temp_array + [0,1,0,0,0,0,0,0]
+            elif bowling_team == 'Punjab Kings':
+                temp_array = temp_array + [0,0,1,0,0,0,0,0]
+            elif bowling_team == 'Kolkata Knight Riders':
+                temp_array = temp_array + [0,0,0,1,0,0,0,0]
+            elif bowling_team == 'Mumbai Indians':
+                temp_array = temp_array + [0,0,0,0,1,0,0,0]
+            elif bowling_team == 'Rajasthan Royals':
+                temp_array = temp_array + [0,0,0,0,0,1,0,0]
+            elif bowling_team == 'Royal Challengers Bangalore':
+                temp_array = temp_array + [0,0,0,0,0,0,1,0]
+            elif bowling_team == 'Sunrisers Hyderabad':
+                temp_array = temp_array + [0,0,0,0,0,0,0,1]
+                
+            # assert batting_team != bowling_team
+            overs = float(request.form['overs'])
+            # assert overs > 5
+            runs = int(request.form['runs'])
+            wickets = int(request.form['wickets'])
+            runs_in_prev_5 = int(request.form['runs_in_prev_5'])
+            # assert runs >= runs_in_prev_5
+            wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+            # assert wickets >= wickets_in_prev_5
+            temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+            
+            data = np.array([temp_array])
+            my_prediction = int(regressor.predict(data)[0])
+            
+            if (batting_team != bowling_team):
+                if (overs > 5  and overs < 20):
+                    if (runs >= runs_in_prev_5):
+                        if (wickets < 10):
+                            if (wickets_in_prev_5 < 10):
+                                if (wickets >= wickets_in_prev_5):
+                                    return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
+                                else :
+                                    return render_template("error4.html")
+                            else:
+                                return render_template("error6.html")
+                        else:
+                                return render_template("error7.html")
+                    else :
+                        return render_template("error3.html")
+                else :
+                    return render_template('error2.html')
+            else : 
+                return render_template("error1.html")      
+        except:
+            return render_template('error5.html')
+                
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", debug=True)
